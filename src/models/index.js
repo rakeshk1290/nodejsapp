14ead7817048d1ff3262056307e9923bf3947782
Rakesh K <rkatta10@in.ibm.com>
2022-12-21 17:45:21 +0530

    010

--- src/models/index.js
+++ src/models/index.js
@@ -1,24 +1,22 @@
-/* eslint-disable import/no-dynamic-require */
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 
 const env = process.env.NODE_ENV || 'development'
 const configPath = path.join(__dirname, '..', 'config', 'dbConfig.json')
+// eslint-disable-next-line security/detect-non-literal-require, import/no-dynamic-require
 const config = require(configPath)[env]
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 const db = {}
+const files = fs.readdirSync(__dirname).filter((file) => file.indexOf('.') !== 0 && file !== 'index.js')
 
-fs.readdirSync(__dirname)
-  .filter(function (file) {
-    return file.indexOf('.') !== 0 && file !== 'index.js'
-  })
-  .forEach(function (file) {
-    // eslint-disable-next-line security/detect-non-literal-require, global-require
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
-    db[model.name] = model
-  })
+files.forEach((file) => {
+  const filePath = path.join(__dirname, file)
+  // eslint-disable-next-line security/detect-non-literal-require, import/no-dynamic-require, global-require
+  const model = file ? require(filePath)(sequelize) : ''
+  db[model.name] = model
+})
 
 Object.keys(db).forEach(function (modelName) {
   if ('associate' in db[modelName]) {
